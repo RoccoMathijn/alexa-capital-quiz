bb5b66147d201543e28b5704c1051dbde57fad6a
rmathijn <rocco.mathijnandela@dhl.com>
2017-02-18 22:05:11 +0100

    Translation changes

--- src/index.js
+++ src/index.js
@@ -18,26 +18,27 @@ var languageString = {
         "translation": {
             "QUESTIONS" : questions,
             "GAME_NAME" : "Capital quiz by Elroy and Rocco", // Be sure to change this for your skill.
-            "HELP_MESSAGE": "I will ask you %s questions. Respond with the correct answer.",
+            "HELP_MESSAGE": "I will ask you %s questions. Respond with the correct answer. ",
             "REPEAT_QUESTION_MESSAGE": "To repeat the last question, say, repeat. ",
             "ASK_MESSAGE_START": "Would you like to start playing? ",
-            "HELP_REPROMPT": "To give an answer to a question, respond with the city name of your choice.",
-            "STOP_MESSAGE": "Would you like to keep playing?",
-            "CANCEL_MESSAGE": "Ok, let\'s play again soon.",
-            "NO_MESSAGE": "Ok, we\'ll play another time. Goodbye!",
-            "TRIVIA_UNHANDLED": "Try saying a number between 1 and %s",
-            "HELP_UNHANDLED": "Say yes to continue, or no to end the game.",
-            "START_UNHANDLED": "Say start to start a new game.",
+            "HELP_REPROMPT": "To give an answer to a question, respond with the city name of your choice. ",
+            "STOP_MESSAGE": "Would you like to keep playing? ",
+            "CANCEL_MESSAGE": "Ok, let\'s play again soon. ",
+            "NO_MESSAGE": "Ok, we\'ll play another time. Goodbye! ",
+            "TRIVIA_UNHANDLED": "Try saying a number between 1 and %s. ",
+            "HELP_UNHANDLED": "Say yes to continue, or no to end the game. ",
+            "START_UNHANDLED": "Say start to start a new game. ",
             "NEW_GAME_MESSAGE": "Welcome to %s. ",
             "WELCOME_MESSAGE": "I will ask you %s questions, try to get as many right as you can. " +
-            "You can run but you can't hide. Bitch! ",
+            "Let the games begin! ",
             "ANSWER_CORRECT_MESSAGE": "correct. ",
             "ANSWER_WRONG_MESSAGE": "wrong. ",
             "CORRECT_ANSWER_MESSAGE": "The correct answer is %s: %s. ",
             "ANSWER_IS_MESSAGE": "That answer is ",
             "TELL_QUESTION_MESSAGE": "Question %s. %s ",
-            "GAME_OVER_MESSAGE": "You got %s out of %s questions correct. Thank you for playing!",
-            "SCORE_IS_MESSAGE": "Your score is %s.",
+            "GAME_OVER_MESSAGE": "You got %s out of %s questions correct. Thank you for playing! " +
+            "Have a nice evening Olaf, Elroy and Rocco. ",
+            "SCORE_IS_MESSAGE": "Your score is %s. ",
             "CAPITAL_QUESTION": "What is the capital of %s? ",
             "CAPITAL_ANSWER": "The capital of %s is %s. "
         }
